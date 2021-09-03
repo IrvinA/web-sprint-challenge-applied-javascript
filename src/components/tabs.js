3ac234da29ae4335a8e13d2cc70671cf8b02c9f9
IrvinA <arevalosirvin@gmail.com>
2021-09-03 05:54:42 -0700

    assignment finished passing all tests

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -37,11 +37,21 @@ const tabsAppender = (selector) => {
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
   const appendPoint = document.querySelector(`${selector}`);
-  axios.get('http://localhost:5000/api/topics')
+  const axios = require('axios');
+  const config = {
+    method: 'get',
+    url: 'http://localhost:5000/api/topics',
+    headers: { }
+  };
+  axios(config)
   .then(resp => {
-    const data = resp.topics;
-    const tabs = Tabs(data);
-    appendPoint.appendChild(tabs);
+    const data = resp.data;
+    const innerData = data.topics;
+    const tab = Tabs(innerData);
+    appendPoint.appendChild(tab);
+  })
+  .catch(err => {
+    console.log(err)
   })
   return appendPoint;
 }
